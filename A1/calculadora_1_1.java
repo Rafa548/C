b72d3d2b4dd0c1e454a733eebd8c9325ce9afc30
Rafa548 <rafael3vilaca30@gmail.com>
2023-02-23 11:52:06 +0000

    ex 1.1 feito

---
+++ A1/calculadora_1_1.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class calculadora_1_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Calculo desejado: ");
+        String calculo = sc.nextLine();
+        String[] calculoSeparado = calculo.split(" ");
+        if (calculoSeparado.length != 3) {
+            System.err.println("Calculo inválido");
+            return;
+        }
+
+        double num1 = Double.parseDouble(calculoSeparado[0]);
+        double num2 = Double.parseDouble(calculoSeparado[2]);
+        String operacao = calculoSeparado[1];
+        double resultado = 0;
+        switch (operacao) {
+            case "+":
+                resultado = num1 + num2;
+                System.out.println("Resultado: " + resultado);
+                break;
+            case "-":
+                resultado = num1 - num2;
+                System.out.println("Resultado: " + resultado);
+                break;
+            case "*":
+                resultado = num1 * num2;
+                System.out.println("Resultado: " + resultado);
+                break;
+            case "/":
+                resultado = num1 / num2;
+                System.out.println("Resultado: " + resultado);
+                break;
+            default:
+                System.err.println("Operação inválida");
+                break;
+        }
+    }
+}
