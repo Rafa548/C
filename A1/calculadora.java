d24cb99528275feccf2ad6c09369e9ece78cd2c3
Rafa548 <rafael3vilaca30@gmail.com>
2023-02-23 11:35:44 +0000

    A1 inicio

---
+++ A1/calculadora.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class calculadora {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Calculo desejado: ");
+        String operacao = sc.nextLine();
+
+    }
+}
